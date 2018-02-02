28bcfd67445bd13a73849412e56a5079fc96ae00
Jeoffrey Thirot <jeoffrey.thirot@gmail.com>
2018-02-02 10:49:46 +0100

    Warning! Dirty commit, maybe you can squash it later... Add default model and start network engine + add bridging header to write my code in swift in ObjC project

---
+++ DeezerExercice/DeezerExercice/NetworkEngine.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkEngine.swift
+//  DeezerExercice
+//
+//  Created by Jeoffrey Thirot on 18/01/2018.
+//  Copyright © 2018 Deezer. All rights reserved.
+//
+
+import UIKit
+
+class NetworkEngine: NSObject {
+
+}
